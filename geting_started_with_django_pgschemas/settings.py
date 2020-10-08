5ef6b713231092dd1d3b1387c955047808376477
Ngahu <ngangangahu@gmail.com>
2020-10-08 17:08:29 +0300

    Create the apps module it is going to contain all the apps for the project.

--- geting_started_with_django_pgschemas/settings.py
+++ geting_started_with_django_pgschemas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django_pgschemas.middleware.TenantMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,13 +75,21 @@ WSGI_APPLICATION = 'geting_started_with_django_pgschemas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django_pgschemas.postgresql_backend",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER", None),
+        "PASSWORD": config("DB_PASSWORD", None),
+        "HOST": config("DB_HOST", None),
+        "PORT": "5432",
+        "CONN_MAX_AGE": 600,
     }
 }
 
+
+DATABASE_ROUTERS = ("django_pgschemas.routers.SyncRouter",)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
