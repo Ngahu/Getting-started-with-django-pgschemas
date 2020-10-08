ba575a1ac777ab197489337a1edd9b1bccf5f51a
Ngahu <ngangangahu@gmail.com>
2020-10-08 22:51:20 +0300

    Create the main apps module.It will contain the apps used for the www tenant.

--- apps/tenant_apps/tenant_app/models.py
+++ apps/tenant_apps/tenant_app/models.py
@@ -1,3 +1,8 @@
+
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class TenantData(models.Model):
+    name = models.CharField(max_length=123)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="tenant_objects")
